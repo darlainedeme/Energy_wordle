ea6b34d1b73a5b43f831e214e0cb40221540ce16
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2024-07-07 10:57:52 +0200

    Update energy_balance_game.py

--- energy_balance_game.py
+++ energy_balance_game.py
@@ -70,7 +70,7 @@ selected_flow = st.selectbox("Select a Flow to investigate:", flows, index=list(
 filtered_data = energy_data[energy_data['Flow'] == selected_flow]
 
 # Filter data for total final consumption only
-tfc_data = energy_data[energy_data['Flow'] == "Total Final Consumption (PJ)"]
+tfc_data = energy_data[energy_data['Flow'] == "Total final consumption (PJ)"]
 
 # Selected country and filtering data
 selected_country = st.session_state.selected_country
